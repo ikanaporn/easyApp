cb57e5799f23b30848264ce004bdce1ce8505311
mai <kanaporn.inc@gmail.com>
2021-05-10 22:42:07 +0700

    old

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,18 +15,45 @@ import {NavigationName} from '../constants';
 
 class HomePage extends Component {
     state = {
-        email:"",
-        password:""
+        title: "title",
+        completed: "YES",
+        createdAt: "TODAY",
+        addingTodo: false,
     }
     constructor(props) {
         super(props);
-       
-    }
-   
-    render() {
         
+    }
+    
+    onSubmit = () => {
+        if (this.state.title.length > 0) this.props.onAdd(this.state);
+        return null;
+      };
+    setStateUtil = (property, value = undefined) => {
+        this.setState({
+          [property]: value,
+        });
+      };
+    
+    render() {
+       
         return (
+           
              <View style={styles.container}>
+                  <TextInput
+                    style={{ width: '90%' }}
+                    placeholder="What needs to be done?"
+                    autoFocus
+                    underLineColorAndroid="transparent"
+                    underlineColor="transparent"
+                    blurOnSubmit
+                    onSubmitEditing={this.onSubmit}
+                    onChangeText={changedTitle => this.setStateUtil('title', changedTitle)}
+                    //value={title}
+                    //autoCorrect={false}
+                    autoCapitalize="none"
+                    //onBlur={onBlur}
+                    />
                  <Text>Login success!</Text>
                     <Fab
                         direction="up"
